0b37f429683922f30c90fbcd587df4452c8583a8
Aditya Kandya <adityakandya2104@gmail.com>
2021-11-23 17:15:05 +0530

    Final Commit Day-2 by Aditya

--- Bakery/models.py
+++ Bakery/models.py
@@ -11,8 +11,9 @@ CATEGORY = (
 class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     quantity = models.PositiveIntegerField(null=True)
-    price = models.FloatField(null=True)
+    price = models.PositiveIntegerField(null=True)
     category = models.CharField(max_length=50, choices=CATEGORY, null=True)
+    image = models.ImageField(upload_to='uploads/products/', default='default_image.jpeg')
 
     def __str__(self):
         return f'{self.name}'
