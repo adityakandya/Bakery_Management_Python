1ad1647aaaf0849b8ef0d60397ba622672f8c36b
Chedvihas <chedvi4999@gmail.com>
2021-11-24 10:07:51 +0530

    Added admin page product details

--- Bakery/admin.py
+++ Bakery/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Product
+from .models import Category
+
+
+class AdminProduct(admin.ModelAdmin):
+	list_display = ['name','quantity','price']
+
+class AdminCategory(admin.ModelAdmin):
+	list_display = ['name']
+
 # Register your models here.
 
 admin.site.site_header = 'Admin Site'
 
-admin.site.register(Product)
+admin.site.register(Product,AdminProduct)
+admin.site.register(Category,AdminCategory)
 # admin.site.register(Order)
--- Bakery/models.py
+++ Bakery/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 
-CATEGORY = (
-    ('Cakes', 'Cakes'),
-    ('Pastry', 'Pastry'),
-    ('Shakes', 'Shakes'),
-    ('Cookies', 'Cookies'),
-)
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     quantity = models.PositiveIntegerField(null=True)
     price = models.PositiveIntegerField(null=True)
-    category = models.CharField(max_length=50, choices=CATEGORY, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
     image = models.ImageField(upload_to='uploads/products/', default='default_image.jpeg')
 
     def __str__(self):
