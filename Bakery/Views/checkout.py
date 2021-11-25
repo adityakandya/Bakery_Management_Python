537b94962753d9df217fc76018f2cc5f926dcade
Aditya Kandya <adityakandya2104@gmail.com>
2021-11-25 22:32:20 +0530

    Added email noti for checkout

--- Bakery/Views/checkout.py
+++ Bakery/Views/checkout.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from Bakery import models
 from django.views import View
 from django.db.models import F
+from django.core.mail import send_mail
+
+
 class Checkout(View):
     def post(self, request):
         address = request.POST.get('address')
@@ -10,6 +13,8 @@ class Checkout(View):
         email = request.session.get('customer_email')
         cart = request.session.get('cart')
         products = models.Product.get_products_by_id(list(cart.keys()))
+        order_str = '\n\n'
+        total = 0
 
         for product in products:
             order = models.Order(customer=models.Customer(id=customer),
@@ -21,6 +26,16 @@ class Checkout(View):
                           quantity=cart.get(str(product.id)))
             models.Product.objects.filter(name=product).update(quantity=F('quantity')-int(cart.get(str(product.id))))
             order.save()
-            
+            quantity = cart.get(str(product.id))
+            total+=(product.price*int(quantity))
+            order_str+=(str(product)+' : '+str(quantity) + '\n')
+        
+        order_str+=('\nTotal = ' + str(total) + '\n\nThank You for your purchase\n\nBakery')
+        send_mail(
+            'Purchase Info',
+            f'You have purchased these items from our shop - {order_str}',
+            'bakerymailpython@gmail.com',
+            [email],
+            fail_silently=False,)
         request.session['cart'] = {}
         return redirect('Bakery-cart')
