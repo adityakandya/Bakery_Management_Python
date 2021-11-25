a4aeb59f0bf6a46ce022ccdf0a74b59fe7e14d12
Sandra Jacob <Sandra.Jacob@lntinfotech.com>
2021-11-25 15:58:34 +0530

    Cart qnty inc_dec fn added

--- Bakery/Views/cart.py
+++ Bakery/Views/cart.py
@@ -9,3 +9,29 @@ class Cart(View):
         products = models.Product.get_products_by_id(ids)
         print(products)
         return render(request, 'Bakery/cart.html',{'products':products})
+
+    def post(self,request):
+        product = request.POST.get('product')
+        limit = models.Product.get_quantity_by_productid(product)
+        remove = request.POST.get('remove')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                if remove:
+                    if quantity<=1:
+                        cart.pop(product)
+                    else:
+                        cart[product] = quantity - 1
+                elif quantity>=int(limit):
+                    cart[product] = limit
+                else:
+                    cart[product] = quantity + 1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+        request.session['cart'] = cart
+        # return render(request, 'Bakery/cart.html',{'products':products})
+        return redirect('Bakery-cart')
