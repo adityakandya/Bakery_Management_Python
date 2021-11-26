fdd073918b6949665f89104e343985f388cc762d
charan <vemulanagacharan@gmail.com>
2021-11-26 10:06:05 +0530

    Fixed Available hrs functionality

--- Bakery/Views/checkout.py
+++ Bakery/Views/checkout.py
@@ -3,6 +3,9 @@ from Bakery import models
 from django.views import View
 from django.db.models import F
 from django.core.mail import send_mail
+from django.contrib import messages
+
+import datetime
 
 
 class Checkout(View):
@@ -15,27 +18,37 @@ class Checkout(View):
         products = models.Product.get_products_by_id(list(cart.keys()))
         order_str = '\n\n'
         total = 0
-
-        for product in products:
-            order = models.Order(customer=models.Customer(id=customer),
-                          email=email,
-                          product=product,
-                          price=product.price,
-                          address=address,
-                          phone=phone,
-                          quantity=cart.get(str(product.id)))
-            models.Product.objects.filter(name=product).update(quantity=F('quantity')-int(cart.get(str(product.id))))
-            order.save()
-            quantity = cart.get(str(product.id))
-            total+=(product.price*int(quantity))
-            order_str+=(str(product)+' : '+str(quantity) + '\n')
+        a = datetime.datetime.now().time()
+        for i in models.Available_hrs.get_all():
+            b = (i.Opening_time)
+            c = (i.Closing_time)
+        if b < a < c:
+           
         
-        order_str+=('\nTotal = ' + str(total) + '\n\nThank You for your purchase\n\nBakery')
-        send_mail(
-            'Purchase Info',
-            f'You have purchased these items from our shop - {order_str}',
-            'bakerymailpython@gmail.com',
-            [email],
-            fail_silently=False,)
-        request.session['cart'] = {}
-        return redirect('Bakery-cart')
+            for product in products:
+                order = models.Order(customer=models.Customer(id=customer),
+                            email=email,
+                            product=product,
+                            price=product.price,
+                            address=address,
+                            phone=phone,
+                            quantity=cart.get(str(product.id)))
+                models.Product.objects.filter(name=product).update(quantity=F('quantity')-int(cart.get(str(product.id))))
+                order.save()
+                quantity = cart.get(str(product.id))
+                total+=(product.price*int(quantity))
+                order_str+=(str(product)+' : '+str(quantity) + '\n')
+            
+            order_str+=('\nTotal = ' + str(total) + '\n\nThank You for your purchase\n\nBakery')
+            send_mail(
+                'Purchase Info',
+                f'You have purchased these items from our shop - {order_str}',
+                'bakerymailpython@gmail.com',
+                [email],
+                fail_silently=False,)
+            request.session['cart'] = {}
+            return redirect('Bakery-cart')
+        else:
+            messages.success(request, 'Shop is Closed!')
+            return redirect('Bakery-cart')
+        
--- Bakery/Views/login.py
+++ Bakery/Views/login.py
@@ -20,15 +20,8 @@ class Login(View):
 			if check_password(password, customer.password):
 				request.session['customer_id']=customer.id
 				request.session['customer_email']=customer.email
-				a = datetime.datetime.now().time()
-				for i in models.Available_hrs.get_all():
-					b = (i.Opening_time)
-					c = (i.Closing_time)
-				if b < a < c:
-					return redirect('Bakery-index')
-				else:
-					messages.success(request, 'Shop is Closed!')
-					return redirect('Bakery-login')
+				return redirect('Bakery-index')
+				
 			else:
 				error_message = 'Email or Password Invalid'
 		else:
